e6fc72af3ac725aa8218cb67124b482129a0fcd4
Yen Cao <yencao@MacBook-Air-von-Yen.local>
2022-11-08 23:06:33 +0100

    init

---
+++ src/main/java/htwberlin/de/planyourtodo/PlanYourToDoAppApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.de.planyourtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanYourToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanYourToDoAppApplication.class, args);
+	}
+
+}
